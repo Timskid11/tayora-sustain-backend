e15acc41d6c5640c5a484a3015b1760356208bc4
Timilehin Oyinlola <147236331+Timskid11@users.noreply.github.com>
2026-06-13 18:07:38 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -14,8 +14,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=[
+        "http://localhost:3000",
+        "https://your-production-domain.com"
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
